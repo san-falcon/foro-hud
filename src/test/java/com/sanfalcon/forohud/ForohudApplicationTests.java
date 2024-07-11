1c8ef7548ed7526f92e7f517f55e27376caecdee
san-falcon <sanfalcon200@gmail.com>
2024-07-11 15:28:23 -0500

    Inicio del proyecto

---
+++ src/test/java/com/sanfalcon/forohud/ForohudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanfalcon.forohud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForohudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
